fcee698239fe24483b883ddbc6a787438a4c9260
QiYueYiya <qiyueyi@pm.me>
2022-12-30 13:33:11 +0800

    update

---
+++ README.md
@@ -0,0 +1,15 @@
+# OpenWrt-Action
+云编译OpenWrt固件，支持x86、360t7
+
+OpenWrt-360T7使用[Hanwckf的OpenWrt源码](https://github.com/hanwckf/immortalwrt-mt798x)
+
+OpenWrt-X86使用[Lean的OpenWrt源码](https://github.com/coolsnowwolf/lede)
+
+OpenWrt-X86U固件带[UA2F](https://github.com/Zxilly/UA2F)设备共享防检测插件
+
+UA2F设备共享防检测插件[使用说明](https://github.com/QiYueYiya/OpenWrt-Action/blob/main/ua2f.md)
+
+[OpenWrt插件对应名称](https://www.right.com.cn/forum/thread-3682029-1-1.html)，仅供参考
+
+# 参考
+[P3TERX/Actions-OpenWrt](https://github.com/P3TERX/Actions-OpenWrt)
---
+++ diy1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add feed sources
+sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
+git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+git clone https://github.com/Zxilly/UA2F package/UA2F
+# theme
+# git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# rm -rf package/luci-theme-argon/README* package/luci-theme-argon/Screenshots/
