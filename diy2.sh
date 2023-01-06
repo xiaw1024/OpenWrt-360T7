2cb37321873a73cccbc77c70d8d89f3fb377dcea
xiawang1024 <xiawang1024@gmail.com>
2023-01-06 15:30:49 +0800

    ✨ feat: 默认ip

--- diy2.sh
+++ diy2.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+###
+ # @Author: xiawang1024
+ # @Date: 2023-01-06 15:29:51
+ # @LastEditTime: 2023-01-06 15:30:17
+ # @LastEditors: xiawang1024
+ # @Description: 
+ # @FilePath: /OpenWrt-360T7/diy2.sh
+ # 工作，生活，健康
+### 
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
@@ -11,6 +20,6 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By QiYueYi $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By Xiaw1024 $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
