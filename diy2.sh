48d28d2531eb8ac9ce7e5f5b5218834fe123e095
yjx1804 <73804454+yjx1804@users.noreply.github.com>
2023-02-27 16:03:41 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -20,6 +20,6 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By Xiaw1024 $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
